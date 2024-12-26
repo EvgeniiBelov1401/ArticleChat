6d0c906764f689714e87eb81ab0135e348166df7
Евгений <evgeniibelov1401@gmail.com>
2024-12-26 11:43:24 +0300

    добаыил в модель User пароль

--- ArticleChat/Models/Db/User.cs
+++ ArticleChat/Models/Db/User.cs
@@ -10,5 +10,9 @@ namespace ArticleChat.Models.Db
         [Required]
         [StringLength(20)]
         public string? Nickname { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public string? Password { get; set; }
     }
 }
