ba415aab14efb9b6fb2dc6780759741b1cad8fe7
Евгений <evgeniibelov1401@gmail.com>
2025-01-23 11:22:42 +0300

    добавил в модели валидацию

--- ArticleChat/Models/Db/Role.cs
+++ ArticleChat/Models/Db/Role.cs
@@ -1,7 +1,10 @@
-﻿namespace ArticleChat.Models.Db
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArticleChat.Models.Db
 {
     public class Role
     {
+        [Key]
         public int Id { get; set; }
         public UserRole UserRole { get; set; }
     }
